5e4a95dfdfea46541911048d7935f1a8d9212703
yemobyte <yemobyte@gmail.com>
2025-12-27 17:14:12 -0800

    Support non-SSL sites, rename package to yemo-sc, and fix docs responsiveness

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/api/screenshot', async (req, res) => {
 
     const browser = await puppeteer.launch({
         headless: 'new',
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--ignore-certificate-errors']
     });
 
     try {
